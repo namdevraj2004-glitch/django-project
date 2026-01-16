deb876cbb3fe36370dd65fc978aded739ead5ec4
Raj Namdev <namdevraj2004@gmail.com>
2026-01-17 01:27:46 +0530

    Add user login activity tracking

--- home/models.py
+++ home/models.py
@@ -10,3 +10,15 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class LoginActivity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
+    login_time = models.DateTimeField(auto_now_add=True)
+    logout_time = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} logged in at {self.login_time}"
